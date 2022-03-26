eb549177654c6c55656093a89e55f72819630dac
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-27 00:45:47 +0900

    first commit

---
+++ crane/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace crane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
