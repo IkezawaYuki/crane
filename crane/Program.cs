4b2ec7f4f22ae40c3a7834445caf1edec4ea9795
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-28 23:10:12 +0900

    mod

--- crane/Program.cs
+++ crane/Program.cs
@@ -2,21 +2,12 @@
 
 namespace crane
 {
-    class Program
+    class Apple
     {
-        static void Main(string[] args)
+        static void Main(string[] arg)
         {
-            Console.WriteLine("Hello World!");
-            string part1 = "ulrimate question";
-            int another = 42;
-            part1 = "aiueo";
-            int a;
-            a = 88;
-            Console.WriteLine(a);
-
-            Console.WriteLine(a + another);
-
-            Console.WriteLine(part1 + another);
+            Console.WriteLine("Hello");
+            Console.WriteLine("Everybody");
         }
     }
 }
