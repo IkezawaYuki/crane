f5ee46ee627f6f9b988174123fb711f4f88a0dcd
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-29 22:49:13 +0900

    mod

--- crane/Program.cs
+++ crane/Program.cs
@@ -6,8 +6,48 @@ namespace crane
     {
         static void Main(string[] arg)
         {
-            Console.WriteLine("Hello");
-            Console.WriteLine("Everybody");
+
+        }
+    }
+
+    public class WorkItem
+    {
+        private static int currentID;
+
+        protected int ID { get; set; }
+        protected string Title { get; set; }
+        protected string Description { get; set; }
+        protected TimeSpan jobLength { get; set; }
+
+        public WorkItem()
+        {
+            ID = 0;
+            Title = "Default title";
+            Description = "Default description";
+            jobLength = new TimeSpan();
+        }
+
+        public WorkItem(string title, string description, TimeSpan jobLen)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.Description = description;
+            this.jobLength = jobLen;
+        }
+
+        static WorkItem() => currentID = 0;
+
+        static int GetNextID() => ++currentID;
+
+        public void Update(string title, TimeSpan jobLen)
+        {
+            this.Title = title;
+            this.jobLength = jobLen;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.ID} - {this.Title}";
         }
     }
 }
