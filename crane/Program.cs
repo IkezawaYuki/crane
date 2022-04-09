ad2850345e37049e7d6b690855e74f4a2204e139
IkezawaYuki <y.ikezawa93@gmail.com>
2022-04-09 23:48:24 +0900

    mod

--- crane/Program.cs
+++ crane/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace crane
@@ -10,6 +11,28 @@ namespace crane
             Coffee cup = PourCoffee();
             var eggs = FryEgg(2);
             var bacon = FryBacon(3);
+            var toastTask = MakeToastWithButterAndJamAsync(2);
+
+            var breakfastTasks = new List<Task> { eggs, bacon, toastTask };
+
+            while (breakfastTasks.Count > 0)
+            {
+                Task finishedTask = Task.WhenAny(breakfastTasks);
+                if (finishedTask == eggs)
+                {
+                    Console.WriteLine("eggs are ready");
+                }
+                else if (finishedTask == bacon)
+                {
+
+                }
+                else if (finishedTask == toastTask)
+                {
+
+                }
+                breakfastTasks.Remove(finishedTask);
+            }    
+
         }
 
         private static Coffee PourCoffee()
@@ -29,9 +52,30 @@ namespace crane
             await Task.Delay(4000);
             return new Bacon();
         }
+
+        static async Task<Toast> MakeToastWithButterAndJamAsync(int number)
+        {
+            var toast = await ToastBreadAsync(number);
+            ApplyButter(toast);
+            ApplyJam(toast);
+            return toast;
+        }
+
+        private static void ApplyJam(Toast toast) =>
+            Console.WriteLine("Putting jam on the toast");
+
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on the toast");
+
+        private static async Task<Toast> ToastBreadAsync(int slices)
+        {
+            await Task.Delay(3000);
+            return new Toast();
+        }
     }
 
     class Coffee { }
     class Egg { }
     class Bacon { }
+    class Toast { }
 }
