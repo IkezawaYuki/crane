4282e71eea4a93981d10e0fc2c08761294ef9d04
IkezawaYuki <y.ikezawa93@gmail.com>
2022-04-10 23:31:47 +0900

    mod

--- crane/Program.cs
+++ crane/Program.cs
@@ -6,73 +6,98 @@ namespace crane
 {
     class Apple
     {
-        public static void Main()
+        static System.Collections.Specialized.StringCollection log = new System.Collections.Specialized.StringCollection();
+        static void Main()
         {
-            Coffee cup = PourCoffee();
-            var eggs = FryEgg(2);
-            var bacon = FryBacon(3);
-            var toastTask = MakeToastWithButterAndJamAsync(2);
-
-            var breakfastTasks = new List<Task> { eggs, bacon, toastTask };
-
-            while (breakfastTasks.Count > 0)
+            string[] drives = System.Environment.GetLogicalDrives();
+            foreach (string dr in drives)
             {
-                Task finishedTask = Task.WhenAny(breakfastTasks);
-                if (finishedTask == eggs)
+                System.IO.DriveInfo di = new System.IO.DriveInfo(dr);
+                if (!di.IsReady)
                 {
-                    Console.WriteLine("eggs are ready");
+                    Console.WriteLine("The drive {0} could not be read", di.Name);
+                    continue;
                 }
-                else if (finishedTask == bacon)
-                {
-
-                }
-                else if (finishedTask == toastTask)
-                {
-
-                }
-                breakfastTasks.Remove(finishedTask);
-            }    
-
+                System.IO.DirectoryInfo rootDir = di.RootDirectory;
+                WalkDirectoryTree(rootDir);
+            }
         }
 
-        private static Coffee PourCoffee()
+        static void WalkDirectoryTree(System.IO.DirectoryInfo root)
         {
-            return new Coffee();
-        }
 
-        private static async Task<Egg> FryEgg(int howMany)
-        {
-            await Task.Delay(3000);
-            Console.WriteLine("cooking the eggs ...");
-            return new Egg();
-        }
-
-        private static async Task<Bacon> FryBacon(int slices)
-        {
-            await Task.Delay(4000);
-            return new Bacon();
-        }
-
-        static async Task<Toast> MakeToastWithButterAndJamAsync(int number)
-        {
-            var toast = await ToastBreadAsync(number);
-            ApplyButter(toast);
-            ApplyJam(toast);
-            return toast;
-        }
-
-        private static void ApplyJam(Toast toast) =>
-            Console.WriteLine("Putting jam on the toast");
-
-        private static void ApplyButter(Toast toast) =>
-            Console.WriteLine("Putting butter on the toast");
-
-        private static async Task<Toast> ToastBreadAsync(int slices)
-        {
-            await Task.Delay(3000);
-            return new Toast();
         }
     }
+    
+    //class Apple
+    //{
+    //    public static void Main(string[] arg)
+    //    {
+    //        Coffee cup = PourCoffee();
+    //        var eggs = FryEgg(2);
+    //        var bacon = FryBacon(3);
+    //        var toastTask = MakeToastWithButterAndJamAsync(2);
+
+    //        var breakfastTasks = new List<Task> { eggs, bacon, toastTask };
+
+    //        while (breakfastTasks.Count > 0)
+    //        {
+    //            Task finishedTask = Task.WhenAny(breakfastTasks);
+    //            if (finishedTask == eggs)
+    //            {
+    //                Console.WriteLine("eggs are ready");
+    //            }
+    //            else if (finishedTask == bacon)
+    //            {
+    //                Console.WriteLine("bacon is ready");
+    //            }
+    //            else if (finishedTask == toastTask)
+    //            {
+    //                Console.WriteLine("toast is ready");
+    //            }
+    //            breakfastTasks.Remove(finishedTask);
+    //        }    
+
+    //    }
+
+    //    private static Coffee PourCoffee()
+    //    {
+    //        return new Coffee();
+    //    }
+
+    //    private static async Task<Egg> FryEgg(int howMany)
+    //    {
+    //        await Task.Delay(3000);
+    //        Console.WriteLine("cooking the eggs ...");
+    //        return new Egg();
+    //    }
+
+    //    private static async Task<Bacon> FryBacon(int slices)
+    //    {
+    //        await Task.Delay(4000);
+    //        return new Bacon();
+    //    }
+
+    //    static async Task<Toast> MakeToastWithButterAndJamAsync(int number)
+    //    {
+    //        var toast = await ToastBreadAsync(number);
+    //        ApplyButter(toast);
+    //        ApplyJam(toast);
+    //        return toast;
+    //    }
+
+    //    private static void ApplyJam(Toast toast) =>
+    //        Console.WriteLine("Putting jam on the toast");
+
+    //    private static void ApplyButter(Toast toast) =>
+    //        Console.WriteLine("Putting butter on the toast");
+
+    //    private static async Task<Toast> ToastBreadAsync(int slices)
+    //    {
+    //        await Task.Delay(3000);
+    //        return new Toast();
+    //    }
+    //}
 
     class Coffee { }
     class Egg { }
