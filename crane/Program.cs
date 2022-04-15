724f0db67f1acca51049fae529e962f104069eda
IkezawaYuki <y.ikezawa93@gmail.com>
2022-04-15 23:48:42 +0900

    mod

--- crane/Program.cs
+++ crane/Program.cs
@@ -11,27 +11,11 @@ namespace crane
     {
         static void Main(string[] args)
         {
-            GetAccountBalance().Wait();
-            Console.ReadLine();
-
+            Execute().Wait();
         }
 
-        static async Task GetAccountBalance()
+        static async Task Execute()
         {
-            var web3 = new Web3();
-            var balance = await web3.Eth.GetBalance.SendRequestAsync("000");
-            Console.WriteLine($"Balance in Wei: {balance.Value}");
-
-            var etherAmount = Web3.Convert.FromWei(balance.Value);
-            Console.WriteLine($"Balance in Ether: {etherAmount}");
-
-            string toAddress = "";
-            var transaction = web3.Eth.GetEtherTransferService()
-                .TransferEtherAndWaitForReceiptAsync(toAddress, 1.11m, 2);
-
-            var managedAccount = new ManagedAccount("", "");
-            var web3ManagedAccount = new Web3(managedAccount);
-
             
         }
     }
